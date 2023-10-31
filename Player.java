2c56b964b3c9fae1e92deaa472bde579164d2830
Anilkumar Sarvabatla <71952099+Mybobksf@users.noreply.github.com>
2023-10-31 11:05:35 -0700

    Update Player.java by adding the player function

--- Player.java
+++ Player.java
@@ -11,7 +11,15 @@ public class Player
   private String name;
   private int points;
   /* your code here - constructor(s) */ 
-
+   public Player() {
+    points = 0;
+    Scanner scanned = new Scanner(System.in);
+    System.out.print("Enter Player Name: ");
+    String newName = scanned.nextLine();
+    
+    name = newName;
+    System.out.println("Welcome to the game " + name);
+  }
   /* your code here - accessor(s) */ 
 
   /* your code here - mutator(s) */ 
