c3476d4dfc8265d69ce2b05c20a2be3fbabdff52
Anilkumar Sarvabatla <71952099+Mybobksf@users.noreply.github.com>
2023-11-03 18:55:57 -0700

    Update Player.java by adding comments

--- Player.java
+++ Player.java
@@ -3,12 +3,15 @@
  * 
  * A Player class the PhraseSolverGame
  */
+//Developed by Lukas SOmwang and Anil Sarvabatla
 import java.util.Scanner;
 
 public class Player
 {
+  //Attributes for the player
   private String name;
   private int points;
+  //Welcoming the player into the game
   public Player() {
     points = 0;
     Scanner scanned = new Scanner(System.in);
@@ -24,6 +27,7 @@ public class Player
     name = Names;
     System.out.println("Hello " + name + " Welcome to the game.");
   }
+  //Setting the name of the Player and the amount of points
   public String getName(){
     return name;
   }
